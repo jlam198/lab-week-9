77c315e696e913ece5f33096edcbc01b24779a27
Jeremy <jlam198@my.bcit.ca>
2021-03-08 12:26:59 -0800

    Week 9 Lab

---
+++ app.js
@@ -0,0 +1,14 @@
+let var_1 = 33
+let var_2 = "JEREMY"
+const var_3 = 55
+const var_4 = true
+
+if (var_1 === var_3) {
+    console.log("they match")
+} else {
+    console.log("no luck")
+}
+
+for (letter of var_2) {
+    console.log(letter)
+}
